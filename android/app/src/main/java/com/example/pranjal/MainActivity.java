0373b0e62c1eb11b3e610ce41b29a735e05c5636
Arjun <arjuntanwar445@gmail.com>
2024-09-06 18:00:44 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/pranjal/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.pranjal;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WebViewExample(),
+      debugShowCheckedModeBanner: false, // Remove the debug banner
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  const WebViewExample({super.key});
+
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the WebViewController
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://freshlifebites.com'));  // Load the URL here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(
+          controller: _controller,  // Use the controller with WebViewWidget
+        ),
+      ),
+    );
+  }
+}
